3fcdb2d491762d06719a3ccdb60de4e31c731bd7
Kevin Medzorian <kevin.medzor@gmail.com>
2019-03-06 02:26:35 -0800

    Renamed GUI file location, name, classname, and components.

---
+++ Communication/base_station/GUI/main.py
@@ -0,0 +1,143 @@
+from Tkinter import *
+from map import Map
+
+TOP_FRAME_HEIGHT = 370
+BOT_FRAME_HEIGHT = 100
+
+# Test Constants
+FONT = "Courier New"
+HEADING_SIZE = 20
+BUTTON_SIZE  = 11
+STATUS_SIZE  = 12
+
+# frame paddings
+PADX = 5
+PADY = 5
+
+# button paddings
+bPADX = 10
+bPADY = 2.5
+
+# button width and heigth (in text units) 
+BUTTON_WIDTH = 17
+BUTTON_HEIGHT = 2
+
+class Main:
+    def __init__(self, master):
+        self.master = master
+        self.master.title("Yonder Arctic OPS")
+
+        self.top_frame = Frame(self.master, bd = 1) 
+        self.top_frame.pack( fill = BOTH, side = TOP, padx = PADX, pady = PADY, expand = YES)
+
+        self.bot_frame = Frame(self.master, bd = 1)
+        self.bot_frame.pack( fill = BOTH, side = BOTTOM, padx = PADX, pady = PADY, expand = YES)
+ 
+        self.init_function_frame()
+        self.init_map_frame()
+        self.init_status_frame()
+        self.init_log_frame()
+        self.init_legend_frame()
+        self.init_config_frame()
+        self.create_function_buttons()
+        self.create_map(self.map_frame)
+
+    # Create the frame for functions
+    def init_function_frame(self):
+        self.functions_frame = Frame(self.top_frame, height = TOP_FRAME_HEIGHT, width = 150, bd = 1, relief = SUNKEN)
+        self.functions_frame.pack( padx = PADX, pady = PADY, side = LEFT, fill=Y, expand = NO)
+        self.functions_frame.pack_propagate(0)
+
+    # Create the frame for the x, y map
+    def init_map_frame(self):
+        self.map_frame = Frame(self.top_frame, height = TOP_FRAME_HEIGHT, width = TOP_FRAME_HEIGHT, bd = 1, relief = SUNKEN)
+        self.map_frame.pack( fill = BOTH, padx = PADX, pady = PADY, side = LEFT, expand = YES)
+        self.map_frame.pack_propagate(0)
+
+    def init_status_frame(self):
+        self.status_frame = Frame(self.top_frame, height = TOP_FRAME_HEIGHT, width = 350, bd = 1, relief = SUNKEN )
+        self.status_frame.pack(fill = BOTH, padx = PADX, pady = PADY, side = LEFT, expand = NO)
+        self.status_frame.pack_propagate(0)
+        self.status_label = Label(self.status_frame, text = "Vehicle Stats", font = (FONT, HEADING_SIZE))
+        self.status_label.pack() 
+        self.status_label.place(relx = 0.22, rely = 0.02)
+        
+        self.position_label_string = StringVar()
+        self.position_label = Label(self.status_frame, textvariable = self.position_label_string, font = (FONT, STATUS_SIZE), justify = LEFT)
+        self.position_label.pack()
+        self.position_label_string.set("Position \n \tX: \t Y: ")
+        self.position_label.place(relx = 0.05, rely = 0.30, anchor = 'sw') 
+
+        self.heading_label_string = StringVar()
+        self.heading_label = Label(self.status_frame, textvariable = self.heading_label_string, font = (FONT, STATUS_SIZE), justify = LEFT)
+        self.heading_label.pack()
+        self.heading_label_string.set("Heading: ")
+        self.heading_label.place(relx = 0.05, rely = 0.40, anchor = 'sw')
+
+        self.battery_status_string = StringVar()
+        self.battery_voltage = Label(self.status_frame, textvariable = self.battery_status_string, font = (FONT, STATUS_SIZE))
+        self.battery_voltage.pack()
+        self.battery_status_string.set("Battery Voltage: ")
+        self.battery_voltage.place(relx = 0.05, rely = 0.50, anchor = 'sw')
+
+        self.vehicle_status_string = StringVar()
+        self.vehicle_status = Label(self.status_frame, textvariable = self.vehicle_status_string, font = (FONT, STATUS_SIZE))
+        self.vehicle_status.pack()
+        self.vehicle_status_string.set("Vehicle Status: Manual Control")
+        self.vehicle_status.place(relx = 0.05, rely = 0.60, anchor = 'sw')
+
+        self.comms_status_string = StringVar()
+        self.comms_status = Label(self.status_frame, textvariable = self.comms_status_string, font = (FONT, STATUS_SIZE))
+        self.comms_status.pack()
+        self.comms_status_string.set("Comms Status: Not connected")
+        self.comms_status.place(relx = 0.05, rely = 0.70, anchor = 'sw')
+
+    def init_log_frame(self):
+        self.log_frame = Frame(self.bot_frame, height = BOT_FRAME_HEIGHT, width = 100, bd = 1, relief = SUNKEN )
+        self.log_frame.pack( fill = BOTH, padx = PADX, pady = PADY, side = LEFT, expand = YES)
+        self.log_frame.pack_propagate(0)
+        #self.log_label = Label(self.log_frame, text = "in log frame").pack()
+
+    def init_legend_frame(self): 
+        self.legend_frame = Frame(self.bot_frame, height = BOT_FRAME_HEIGHT, width = 100, bd = 1, relief = SUNKEN)
+        self.legend_frame.pack( fill = BOTH, padx = PADX, pady = PADY, side = LEFT, expand = YES)
+        self.legend_frame.pack_propagate(0)
+        #self.legend_label = Label(self.legend_frame, text = "in legend frame").pack()
+
+    def init_config_frame(self):
+        self.config_frame = Frame(self.bot_frame, height = BOT_FRAME_HEIGHT, width = 100, bd = 1, relief = SUNKEN)
+        self.config_frame.pack( fill = BOTH, padx = PADX, pady = PADY, side = LEFT, expand = YES)
+        self.config_frame.pack_propagate(0)
+        #self.config_label = Label(self.config_frame, text = "in config frame").pack() 
+
+    def create_function_buttons(self):
+        self.origin_button           = Button(self.functions_frame, text = "Set Origin", takefocus = False, width = BUTTON_WIDTH, height = BUTTON_HEIGHT,
+                                              padx = bPADX, pady = bPADY, font = (FONT, BUTTON_SIZE), command = lambda: None)
+        self.add_waypoint_button     = Button(self.functions_frame, text = "Add Waypoint", takefocus = False, width = BUTTON_WIDTH, height = BUTTON_HEIGHT, 
+                                              padx = bPADX, pady = bPADY, font = (FONT, BUTTON_SIZE), command = lambda: None)
+        self.nav_to_waypoint_button  = Button(self.functions_frame, text = "Nav. to Waypoint", takefocus = False, width = BUTTON_WIDTH, height = BUTTON_HEIGHT,
+                                              padx = bPADX, pady = bPADY, font = (FONT, BUTTON_SIZE), command = lambda: None)
+        self.ballast_button          = Button(self.functions_frame, text = "Start Ballast", takefocus = False, width = BUTTON_WIDTH, height = BUTTON_HEIGHT,
+                                              padx = bPADX, pady = bPADY, font = (FONT, BUTTON_SIZE), command = lambda: None)
+        self.switch_to_manual_button = Button(self.functions_frame, text = "Switch to Manual", takefocus = False, width = BUTTON_WIDTH, height = BUTTON_HEIGHT,
+                                              padx = bPADX, pady = bPADY, font = (FONT, BUTTON_SIZE), command = lambda: None)
+        self.stop_manual_button      = Button(self.functions_frame, text = "Stop Manual", takefocus = False, width = BUTTON_WIDTH, height = BUTTON_HEIGHT,
+                                              padx = bPADX, pady = bPADY, font = (FONT, BUTTON_SIZE), command = lambda: None)
+        self.abort_button            = Button(self.functions_frame, text = "ABORT MISSION", takefocus = False, width = BUTTON_WIDTH, height = BUTTON_HEIGHT,
+                                              padx = bPADX, pady = bPADY, bg = 'red', font = (FONT, BUTTON_SIZE), command = lambda: None)
+
+        self.origin_button.pack(expand=YES, fill=BOTH, pady = 3)
+        self.add_waypoint_button.pack(expand=YES, fill=BOTH, pady = 3)
+        self.nav_to_waypoint_button.pack(expand=YES, fill=BOTH, pady = 3)
+        self.ballast_button.pack(expand=YES, fill=BOTH, pady = 3)
+        self.switch_to_manual_button.pack(expand=YES, fill=BOTH, pady = 3)
+        self.stop_manual_button.pack(expand=YES, fill=BOTH, pady = 3)
+        self.abort_button.pack(expand=YES, fill=BOTH, pady = 3)
+        
+    def create_map(self, frame):
+        self.map = Map(frame)        
+
+root = Tk()
+root.geometry("1200x600") 
+Main = Main(root)
+root.mainloop()
---
+++ Communication/base_station/GUI/map.py
@@ -0,0 +1,344 @@
+import matplotlib
+import matplotlib.axes
+matplotlib.use('TkAgg')
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from matplotlib.figure import Figure
+from matplotlib.lines import Line2D
+from matplotlib.pyplot import scatter
+from Tkinter import *
+
+# Object & Map Constants
+DEFAULT_FIGURE_SIZE =  30 # Window Size
+DEFAULT_GRID_SIZE   = 1000 # Grid Size in Meters
+
+# String Constants
+KILOMETERS = "Kilometers (km)"
+METERS     = "Meters (m)"
+MILES      = "Miles (mi)"
+
+# Color Constants
+BACKGROUND_COLOR = 'darkturquoise'
+AUV_PATH_COLOR   = 'red'
+WAYPOINT_COLOR   = 'red'
+MINOR_TICK_COLOR = 'black'
+
+# Conversion Multiplier Constants
+KM_TO_M  = 1000.000000000
+MI_TO_M  = 1609.340000000
+KM_TO_MI = 0000.621371000
+M_TO_MI  = 0000.000621371
+MI_TO_KM = 0001.609340000
+M_TO_KM  = 0000.001000000
+
+# Other Debug Constants
+ZOOM_SCALAR  = 1.15
+CLOSE_ENOUGH = 0.25
+
+
+class Map:
+    def __init__(self,  window):
+        # Define the window.
+        self.window = window
+
+        # Initialize object data/information
+        self.waypoints      = list()
+        self.units          = METERS
+        self.size           = DEFAULT_GRID_SIZE  
+        self.zero_offset_x  = 0
+        self.zero_offset_y  = 0
+        self.old_position   = 0     # Used to move the map whenever the boat moves.
+        self.press_position = [0,0]
+        self.mouse_pressing = False
+        self.legend_obj     = None
+        self.auv_path_obj   = None
+        self.auv_data       = [list(), list()]
+        
+        # Inialize the Tk-compatible Figure, the map, and the canvas
+        self.fig    = self.init_fig()
+        self.map    = self.init_map()
+        self.canvas = self.init_canvas()        
+
+        # Start listening for mouse-clicks
+        self.fig.canvas.mpl_connect('button_press_event',   self.on_press)  
+        self.fig.canvas.mpl_connect('button_release_event', self.on_release)
+        self.fig.canvas.mpl_connect('motion_notify_event',  self.on_move)
+        self.fig.canvas.mpl_connect('scroll_event',         self.on_map_scroll)  
+    
+        # Assign default values.
+        self.set_range() # Set to default range
+
+        # Re-draw the canvas.
+        self.draw_canvas()
+    
+        # Add random data to test line/path functionality.
+        self.add_auv_data(100,100)
+        self.add_auv_data(110,105)
+        self.add_auv_data(130, 109)
+        self.add_auv_data(200,225)
+        self.add_auv_data(240,250)
+        self.add_auv_data(350, 330)
+        self.add_auv_data(360, 200)
+        self.add_auv_data(370, 260)
+        self.add_auv_data(360, 230)
+        self.add_auv_data(350, 200)
+
+        self.clear()
+
+    def clear(self):
+        self.clear_waypoints()
+        self.clear_auv_path()
+        self.draw_canvas()
+
+    def clear_auv_path(self):
+        self.auv_path_obj.pop(0).remove()
+
+    def undraw_waypoints(self):
+        for waypoint in self.waypoints:
+            # Remove waypoint from map.
+            if waypoint[3] != None and type(waypoint[3]) != tuple:
+                waypoint[3].pop(0).remove() 
+                waypoint[3] = None
+            
+            if waypoint[4] != None:
+                waypoint[4].remove()
+                waypoint[4] = None
+
+    def clear_waypoints(self):
+        self.undraw_waypoints()
+        del self.waypoints[:] 
+    
+    def zero_map(self, x=0, y=0):
+        self.zero_offset_x = x
+        self.zero_offset_y = y
+
+    def on_move(self, mouse):
+        if self.mouse_pressing == True and mouse.xdata != None and mouse.ydata != None:
+            x_delta = (self.press_position[0] - mouse.xdata)/6
+            y_delta = (self.press_position[1] - mouse.ydata)/6
+
+            self.map.set_xlim( self.map.get_xlim()[0] + x_delta, self.map.get_xlim()[1] + x_delta )
+            self.map.set_ylim( self.map.get_ylim()[0] + y_delta, self.map.get_ylim()[1] + y_delta )
+            
+            self.draw_canvas()
+
+    def redraw_waypoints(self):
+        self.undraw_waypoints()
+        for waypoint in self.waypoints:
+            # Draw waypoint again.
+            waypoint[3] = self.map.plot(waypoint[0], waypoint[1], marker='o',markersize=5, color="red"),
+            waypoint[4] = self.map.annotate(xy=(waypoint[0], waypoint[1]), 
+                          s=waypoint[2] + " ("+str(round(waypoint[0]+self.zero_offset_x,5))+","+str(round(waypoint[1]+self.zero_offset_y,5))+")" )
+            
+        # Redraw canvas.
+        self.draw_canvas()
+        print("Waypoints Redrawn!")
+   
+    def on_press(self, mouse):
+        self.press_position = [mouse.xdata, mouse.ydata]
+        self.mouse_pressing = True
+    
+    def on_release(self, mouse):
+        self.mouse_pressing = False
+        if mouse.xdata != None and mouse.xdata - self.press_position[0] == 0: # Same x as the press.
+            if mouse.ydata != None and mouse.ydata - self.press_position[1] == 0: # Same y as the press.
+                self.on_map_click(mouse)
+    
+    def on_map_scroll(self, mouse):
+        if mouse.button == 'down': # down => Scroll down
+            self.zoom_out()
+        if mouse.button == 'up':   # up   =>  Scroll up
+            self.zoom_in()
+ 
+    def on_map_click(self, mouse):
+        if mouse.button == 3: # 3 => Right mouse click
+            self.new_waypoint_prompt(mouse.xdata, mouse.ydata)
+        if mouse.button == 1: # 1 => Left mouse click
+            self.try_remove_waypoint(mouse.xdata, mouse.ydata)
+
+    def update_boat_position(self, x = 0, y = 0):
+        return
+
+    def try_remove_waypoint(self, x = 0, y = 0):
+        close = CLOSE_ENOUGH * (self.size / DEFAULT_GRID_SIZE)
+
+        if self.units == METERS:
+            close += 100
+        
+        for waypoint in self.waypoints:
+            if x - close < waypoint[0] and x + close  > waypoint[0]: # Close enough on x-axis.
+                if y - close < waypoint[1] and y + close > waypoint[1]: # Close enough on y-axis.
+                    self.remove_waypoint_prompt(waypoint)
+                    return
+    
+    def remove_waypoint_prompt(self, waypoint):
+        print("Opening remove-waypoint prompt.")
+        prompt_window = Tk()
+        prompt_window.title("Remove Waypoint \""+ str(waypoint[2]) + "\"?");
+        prompt_window.wm_attributes('-type', 'dialog')
+        prompt_submit = Button(prompt_window, text="Yes, I want to remove waypoint \""+str(waypoint[2])+"\"", command=lambda:[self.confirm_remove_waypoint(waypoint), prompt_window.destroy()])
+        prompt_submit.pack(padx=5, pady=5)
+        prompt_window.mainloop()
+
+    def confirm_remove_waypoint(self, waypoint):
+        self.waypoints.remove(waypoint)
+        waypoint[3].pop(0).remove() 
+        waypoint[4].remove();
+        self.draw_canvas()
+        print ("Waypoint removed!")
+        return;
+
+    def new_waypoint_prompt(self, x = 0, y = 0):
+        print("Opening new-waypoint prompt.")
+        prompt_window = Tk()
+        prompt_window.title("New Waypoint");
+        prompt_window.wm_attributes('-type', 'dialog')
+        prompt_input  = Entry(prompt_window, bd=5)
+        prompt_input.insert(0, 'My Waypoint') # Placeholder for input
+        prompt_submit = Button(prompt_window, text="Save", command=lambda:[self.add_waypoint(x, y, str(prompt_input.get())), prompt_window.destroy()])
+        
+        prompt_input.pack(side=LEFT, padx=5, pady=5)
+        prompt_submit.pack(side=RIGHT, padx=5, pady=5)
+        prompt_window.mainloop();
+
+    def add_auv_data(self, x=0, y=0):
+        print("Adding UAV data at: ("+str(x)+", "+str(y)+").")
+        self.auv_data[0].append(x)
+        self.auv_data[1].append(y)
+        self.draw_auv_path()
+
+    def draw_auv_path(self):
+        print("Drawing UAV path.")
+      
+        if self.auv_path_obj != None:
+            self.auv_path_obj.pop(0).remove()
+
+        self.auv_path_obj = self.map.plot(self.auv_data[0], self.auv_data[1], label="AUV Path", color=AUV_PATH_COLOR)
+        
+        # Re-draw the canvas.
+        self.draw_canvas()
+    
+    def draw_canvas(self):
+        return self.canvas.draw()        
+
+    def init_canvas(self):
+        print("Initializing the canvas.")
+
+        # Remove excess borders around figure.
+        self.fig.subplots_adjust( left = 0, bottom = 0, right = 1, top = 1, wspace = 0, hspace = 0 )
+        
+        # Create a tkinter-usable component.
+        canvas = FigureCanvasTkAgg(self.fig, master=self.window) 
+        canvas.get_tk_widget().pack()
+        return canvas
+
+    def init_fig(self):
+        print ("Initializing figure...")
+        fig = Figure(figsize=(DEFAULT_FIGURE_SIZE, DEFAULT_FIGURE_SIZE))
+        return fig 
+
+    def init_map(self):
+        print("Initializing map...")
+        graph = self.fig.add_subplot(111, xmargin = -0.49, ymargin = -0.49)
+        graph.grid(b=True, which='major', axis='both')
+        
+        graph.spines['left'].set_position(('data', 0))
+
+        # turn off the right spine/ticks
+        graph.spines['right'].set_color('none')
+        graph.yaxis.tick_left()
+
+        # set the y-spine
+        graph.spines['bottom'].set_position(('data', 0))
+
+        # turn off the top spine/ticks
+        graph.spines['top'].set_color('none')
+        graph.xaxis.tick_bottom()
+
+        graph.set_facecolor(BACKGROUND_COLOR)
+
+        # Setup the Legend
+        legend_elements = [Line2D([0], [0], color=AUV_PATH_COLOR, lw=2, label='AUV Path'),
+                           scatter([0], [0], marker='o', color=WAYPOINT_COLOR, label='Waypoint')]
+        
+        graph.legend(handles=legend_elements, loc='lower right', title="Legend")
+
+        # Change color of minor axis.
+        graph.tick_params(axis='x', colors=MINOR_TICK_COLOR)
+        graph.tick_params(axis='y', colors=MINOR_TICK_COLOR)
+        
+        return graph
+
+    def add_waypoint(self, x=0, y=0, label="My Waypoint"):
+        print( "Adding waypoint " + label + " at position (" + str(x) + ", " + str(y) + ").\n"+
+               " It's longitude is "+ str(x+self.zero_offset_x))
+
+        self.waypoints.append( [
+                                x,y,
+                                label,
+                                self.map.plot(x, y, marker='o',markersize=5, color=WAYPOINT_COLOR, label=label),
+                                self.map.annotate(xy=(x, y), s=label + " ("+str(round(x+self.zero_offset_x,5))+","+str(round(y+self.zero_offset_y,5))+")" )
+                                ] )
+        
+        self.draw_canvas()
+        return [x,y]
+
+    def zoom_out(self):
+        print("Zooming out.")
+        xlim = self.map.get_xlim()
+        ylim = self.map.get_ylim()
+        self.size *= ZOOM_SCALAR
+        self.set_range(x=[xlim[0]*ZOOM_SCALAR, xlim[1]*ZOOM_SCALAR],
+                       y=[ylim[0]*ZOOM_SCALAR, ylim[1]*ZOOM_SCALAR])
+
+    def zoom_in(self):
+        print("Zooming in.")
+        xlim = self.map.get_xlim()
+        ylim = self.map.get_ylim()
+        self.size /= ZOOM_SCALAR
+        self.set_range(x=[xlim[0]/ZOOM_SCALAR, xlim[1]/ZOOM_SCALAR], 
+                       y=[ylim[0]/ZOOM_SCALAR, ylim[1]/ZOOM_SCALAR])
+
+    def set_range(self, x=[-DEFAULT_GRID_SIZE, DEFAULT_GRID_SIZE], y=[-DEFAULT_GRID_SIZE, DEFAULT_GRID_SIZE]):
+        print("Changing grid size to x="+str(x)+", and y="+str(y)+".") 
+        self.map.set_xlim(x)
+        self.map.set_ylim(y)
+        self.draw_canvas()
+
+    def set_units(self, unit=METERS):
+        print("Changing units from " + self.units + " to "  + unit)
+        multiplier = 1
+
+        # Convert KM -> M
+        if unit == METERS and self.units == KILOMETERS:
+            multiplier = KM_TO_M
+        
+        # Convert MI -> M
+        if unit == METERS and self.units == MILES:
+            multiplier = MI_TO_M
+
+        # Convert KM -> MI
+        if unit == MILES and self.units == KILOMETERS:
+            multiplier = KM_TO_MI
+        
+        # Convert M -> MI
+        if unit == MILES and self.units == METERS:
+            multiplier = M_TO_MI
+
+        # Convert MI -> KM
+        if unit == KILOMETERS and self.units == MILES:
+            multiplier = MI_TO_KM
+        
+        # Convert M -> KM
+        if unit == KILOMETERS and self.units == METERS:
+            multiplier = M_TO_KM
+      
+
+        # Apply conversion
+        for waypoint in self.waypoints:
+            waypoint[0] *= multiplier
+            waypoint[1] *= multiplier
+
+        self.size *= multiplier
+        self.units = unit
+        self.set_range(x=self.size, y=self.size)
+        self.draw_canvas()

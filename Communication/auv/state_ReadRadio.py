adb49e1af8ca37a6ddc361cc6be06161161131fe
Eric Xiao <alpha2275@gmail.com>
2019-04-24 20:30:11 -0700

    Created state_ReadRadio.py

---
+++ Communication/auv/state_ReadRadio.py
@@ -0,0 +1,12 @@
+from state import State
+
+
+class ReadRadio(State):
+    def __init__(self):
+        print('Reading from Radio')
+
+    def handle(self, auv):
+        raise NotImplementedError('INIT states shouldn\'t be called as next state')
+
+    def get_state_name(self):
+        return 'ReadRadio'

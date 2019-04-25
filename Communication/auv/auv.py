d135783948a0459457cb1f1df14b2fcf50526631
Scott Zhao <zhaomh1998@outlook.com>
2019-04-24 21:00:03 -0700

    update logging for state machine

--- Communication/auv/auv.py
+++ Communication/auv/auv.py
@@ -1,37 +1,63 @@
 import state
 import sensors
-import logging, sys
+import logging
+import os
+import sys
 
-logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+import state_Connect
+import state_WaitForAction
 
-filehandler = logging.FileHandler('mylog.log')
-formatter = logging.Formatter("%(asctime)s - %(name)s - %(funcName)s - %(levelname)s - %(message)s") # set format
+# Configure Logging
+log_file_name = "mylog"
+if not os.path.exists("log/"):
+    os.makedirs("log/")
 
-logging.debug("debug message") # no output
-logging.info("info message")
-logging.error("error")
+logger = logging.getLogger('AUV_Root')
+logger.setLevel(logging.DEBUG)
+# create file handler which logs even debug messages
+fh = logging.FileHandler("log/{0}.log".format(log_file_name))
+fh.setLevel(logging.DEBUG)
+# create console handler with a higher log level
+ch = logging.StreamHandler(sys.stdout)
+ch.setLevel(logging.DEBUG)
+# create formatter and add it to the handlers
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(funcName)s - %(levelname)s - %(message)s")
+ch.setFormatter(formatter)
+fh.setFormatter(formatter)
+# add the handlers to logger
+logger.addHandler(ch)
+logger.addHandler(fh)
+
+AUV_STATES = {
+    'INIT': (state_Connect.Connect, 'CONNECT')
+}
 
 
 class AUV():
     def __init__(self):
+        logger.debug("AUV Initializing")
         self.states = dict()
         self.next_state = 'INIT'
         self.sensors = None
-        logging.info('AUV Started')
+        logger.info("AUV Started")
 
     def add_state(self, adding_state):
-        assert adding_state.get_state_name() not in self.states.keys(), 'Cannot add ' + adding_state.get_state_name() + 'that already exist in state list!'
+        assert adding_state not in self.states.keys(), 'Cannot add ' + adding_state.get_state_name() + 'that already exist in state list!'
+        assert adding_state in AUV_STATES.keys(), 'State ' + adding_state + ' not found!'
+        self.states[adding_state] = AUV_STATES[adding_state][0](self)
+        self.next_state = AUV_STATES[adding_state][1]
 
     def run_forever(self):
         while True:
-            self.next_state = self.states[self.next_state].handle(self)
             if self.next_state not in self.states.keys():
                 self.add_state(self.next_state)
+            else:
+                self.next_state = self.states[self.next_state].handle(self)
 
 
 if __name__ == '__main__':
-    auv = AUV(debug=True)
+    auv = AUV()
     try:
         auv.run_forever()
-    except:
-        print('Exception!')
+    except Exception as e:
+        logger.error("AUV_Root returns uncault Error!!", exc_info=True)

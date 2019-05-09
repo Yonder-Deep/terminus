9c7a4a53107e602699f9da30355ea8d564ab48a8
Scott Zhao <zhaomh1998@outlook.com>
2019-05-08 21:26:29 -0700

    command processor

--- Communication/auv/command_processor.py
+++ Communication/auv/command_processor.py
@@ -1,18 +1,28 @@
-CALIBRATE_MOTOR_KW = ['LEFT', '']
+def calibrate_motor(command_dict):
+    return {'last_state': 'READ',
+            'next_state': 'CAL',
+            'data': command_dict}
 
-def calibrate_motor(remaining_param):
-    assert isinstance(remaining_param, list)
-    assert len(remaining_param) == 1
-    assert remaining_param[0] in CALIBRATE_MOTOR_KW
 
-def manual_control(remaining_param):
-    pass
+def manual_control(command_dict):
+    return {'last_state': 'READ',
+            'next_state': 'MAN',
+            'data': command_dict}
 
-def ballast(remaining_param):
-    pass
 
-def auto_navigate(remaining_param):
-    pass
+def ballast(command_dict):
+    return {'last_state': 'READ',
+            'next_state': 'BAL',
+            'data': command_dict}
 
-def echo_status(remaining_param):
-    pass
+
+def auto_navigate(command_dict):
+    return {'last_state': 'READ',
+            'next_state': 'NAV',
+            'data': command_dict}
+
+
+def echo_status(command_dict):
+    return {'last_state': 'READ',
+            'next_state': 'STATUS',
+            'data': command_dict}

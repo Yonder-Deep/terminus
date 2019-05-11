22412900a26ee592937248cae2ba0f980a3d4162
Scott Zhao <zhaomh1998@outlook.com>
2019-05-11 11:26:28 -0700

    json io class

--- Communication/auv/command_io.py
+++ Communication/auv/command_io.py
@@ -2,6 +2,7 @@ import json
 import command_validate
 import traceback
 import collections
+import os
 
 CMD_VALIDATE = {'CAL': command_validate.cal_checker,
                 'MAN': command_validate.man_checker,
@@ -10,54 +11,60 @@ CMD_VALIDATE = {'CAL': command_validate.cal_checker,
                 'ALIVE': command_validate.alive_checker,
                 'STATUS': command_validate.status_checker}
 
+# Sets the PYTHONPATH to include the components.
+split_path = os.path.abspath(__file__).split('/')
+split_path_communication = split_path[0:len(split_path) - 2]
+split_path_sensors = split_path[0:len(split_path) - 3]
 
-def send_calibrate(l=True, r=True, f=True, b=True):
-    packet = {'cmd': 'CAL',
-              'l': l,
-              'r': r,
-              'f': f,
-              'b': b}
-    send_dict(packet)
+components_path = "/".join(split_path_communication) + "/components"
 
 
-def send_manual_control(left, right, forward, back):
-    packet = {'cmd': 'MAN',
-              'l': left,
-              'r': right,
-              'f': forward,
-              'b': back}
-    send_dict(packet)
+class CommandIO:
 
+    def __init__(self, radio):
+        self.radio = radio
 
-def send_ballast(target_depth, timeout=60):
-    packet = {'cmd': 'BAL',
-              'depth': target_depth,
-              'timeout': timeout}
-    send_dict(packet)
+    def send_calibrate(self, l=True, r=True, f=True, b=True):
+        packet = {'cmd': 'CAL',
+                  'l': l,
+                  'r': r,
+                  'f': f,
+                  'b': b}
+        self.send_dict(packet)
 
+    def send_manual_control(self, left, right, forward, back):
+        packet = {'cmd': 'MAN',
+                  'l': left,
+                  'r': right,
+                  'f': forward,
+                  'b': back}
+        self.send_dict(packet)
 
-def send_nav(target_lat, target_lon):
-    packet = {'cmd': 'NAV',
-              'lat': target_lat,
-              'lon': target_lon}
-    send_dict(packet)
+    def send_ballast(self, target_depth, timeout=60):
+        packet = {'cmd': 'BAL',
+                  'depth': target_depth,
+                  'timeout': timeout}
+        self.send_dict(packet)
 
+    def send_nav(self, target_lat, target_lon):
+        packet = {'cmd': 'NAV',
+                  'lat': target_lat,
+                  'lon': target_lon}
+        self.send_dict(packet)
 
-def send_alive():
-    packet = {'cmd': 'ALIVE'}
-    send_dict(packet)
+    def send_alive(self):
+        packet = {'cmd': 'ALIVE'}
+        self.send_dict(packet)
 
+    def send_status(self, current_lat, current_lon, current_state):  # For AUV only
+        packet = {'cmd': 'STATUS',
+                  'lat': current_lat,
+                  'lon': current_lon,
+                  'state': current_state}
+        self.send_dict(packet)
 
-def send_status(current_lat, current_lon, current_state):  # For AUV only
-    packet = {'cmd': 'STATUS',
-              'lat': current_lat,
-              'lon': current_lon,
-              'state': current_state}
-    send_dict(packet)
-
-
-def send_dict(a_dict):
-    print(encode_json(a_dict))
+    def send_dict(self, a_dict):
+        self.radio.write(encode_json(a_dict) + '\n')
 
 
 def encode_json(a_dict):
@@ -82,12 +89,12 @@ def decode_json(a_json_str):
         traceback.print_exc()
 
 
-def dict_unicode_to_string(data):
+def dict_unicode_to_string(self, data):
     if isinstance(data, basestring):
         return str(data)
     elif isinstance(data, collections.Mapping):
-        return dict(map(dict_unicode_to_string, data.iteritems()))
+        return dict(map(self.dict_unicode_to_string, data.iteritems()))
     elif isinstance(data, collections.Iterable):
-        return type(data)(map(dict_unicode_to_string, data))
+        return type(data)(map(self.dict_unicode_to_string, data))
     else:
         return data

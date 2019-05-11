3104802317bee9e7442a2c2feaf41c8582dcf25e
Scott Zhao <zhaomh1998@outlook.com>
2019-05-11 14:06:26 -0700

    added prints to each motor

--- Communication/auv/state_ManualCtrl.py
+++ Communication/auv/state_ManualCtrl.py
@@ -7,6 +7,12 @@ class ManualCtrl(State):
 
     def handle(self, auv):
         state_before_read = auv.next_state['last_state']
+        state_data = auv.next_state['data']
+        left_motor = state_data['l']
+        right_motor = state_data['r']
+        front_motor = state_data['f']
+        back_motor = state_data['b']
+        print("[MAN]", left_motor, right_motor, front_motor, back_motor)
         print(str(auv.next_state))
         return {'last_state': 'READ',
                 'next_state': state_before_read,

c4a5d375e582de1f035a03db0d970dffb53e3126
Scott Zhao <zhaomh1998@outlook.com>
2019-05-11 14:02:31 -0700

    fix#7

--- Communication/auv/state_ManualCtrl.py
+++ Communication/auv/state_ManualCtrl.py
@@ -6,7 +6,11 @@ class ManualCtrl(State):
         self.handle(auv)
 
     def handle(self, auv):
+        state_before_read = auv.next_state['last_state']
         print(str(auv.next_state))
+        return {'last_state': 'READ',
+                'next_state': state_before_read,
+                'data': dict()}
 
     def get_state_name(self):
         return 'MAN'

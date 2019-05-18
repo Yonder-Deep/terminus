a56aea3a360bd9b3912e9780d64d73d36cd1e92e
Scott Zhao <zhaomh1998@outlook.com>
2019-05-18 11:42:13 -0700

    BAL state wip

--- Communication/auv/state_Ballast.py
+++ Communication/auv/state_Ballast.py
@@ -1,22 +1,37 @@
 from state import State
 import time
 
+BALLAST_SPEED = 25
+
 
 class Ballast(State):
     def __init__(self, auv):
         self.target_depth = auv.state_info['data']['depth']
         self.timeout = auv.state_info['data']['timeout']
         self.start_time = time.time()
-        self.mc.update_motor_speeds(left=0, right=0, front=0, back=0)
+        self.mc.update_motor_speeds(left=0, right=0, front=BALLAST_SPEED, back=BALLAST_SPEED)
 
     def handle(self, auv):
         current_depth = auv.pressure_sensor.depth()
         if current_depth < self.target_depth:
-            # Continue ballasting
-            pass
-        hold_state = auv.state_info['hold_state']
-        return {'hold_state': hold_state,
-                'next_state': hold_state,
+            print("[BAL] At depth " + str(current_depth))
+            hold_state = auv.state_info['hold_state']
+            return {'hold_state': 'BAL',
+                    'next_state': 'READ',
+                    'data': dict()}
+
+        elif current_depth >= self.target_depth:
+            # End ballasting
+            print("Done ballasting")
+
+        elif time.time() - self.start_time > self.timeout:
+            print("[BAL] TIME OUT!!")
+            # TODO: If the depth doesn't change by 1m for 10s, reverse motor to come back up
+
+        # BAL state done
+        self.mc.update_motor_speeds(left=0, right=0, front=0, back=0)
+        return {'hold_state': 'READ',
+                'next_state': 'READ',
                 'data': dict()}
 
     def get_state_name(self):
